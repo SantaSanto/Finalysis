ed31cb1d74b59dee2dbd6e354ea79177e63c4b04
SantaSanto <SanthoshKumar.Mail@Gmail.com>
2017-05-26 13:52:11 +0530

    Update Financial.js

--- Financial.js
+++ Financial.js
@@ -1,3 +1,14 @@
 function sayHello() {
   alert(1);
 }
+
+function sum(arrayObj, fieldName) {
+  var arrayObjLen = arrayObj.length;
+  
+  var result = 0;
+  for(var i=0; i<arrayObjLen; i++) {
+    result = result + arrayObj[i].field(fieldName);
+  }
+  
+  return result;
+}
