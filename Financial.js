53cee5e0ce0a552da92c89941e686f670f108612
SantaSanto <SanthoshKumar.Mail@Gmail.com>
2017-05-31 12:08:17 +0530

    Update Financial.js

--- Financial.js
+++ Financial.js
@@ -11,6 +11,18 @@ function minLink(linkName, fieldName) {
   return result;
 }
 
+function CAGRLink(linkName, fieldName) {
+  var linkObjs = field(linkName);
+  var linkLen = linkObjs.length;
+
+  var numOfPeriods = linkLen-1;
+  var beginningValue = linkObjs[numOfPeriods].field(fieldName);
+  var endingValue = linkObjs[0].field(fieldName);
+
+  var CAGR = Math.pow((endingValue / beginningValue), 1 / numOfPeriods) - 1;
+  return Math.round(CAGR * 10000) / 100;
+}
+
 function sum(arrayObj, fieldName) {
   var arrayObjLen = arrayObj.length;
   
